1383bd0cc88c0d0cdd58ff16f1799c30b6308b40
alenmemenduf <47917027+alenmemenduf@users.noreply.github.com>
2020-10-02 13:35:04 +0300

    Added 2 Materials for the player

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,12 +6,15 @@ using Cinemachine;
 
 public class PlayerController : MonoBehaviour
 {
+    public float timeToSwitchShoulder = 0.5f;
+    public GameObject pivot;
     [Header("Player speed")]
     public float acceleration = 2f;
-    public float decceleration = 1f;
     public float maxWalkingVelocity = 0.5f;
     public float maxRunningVelocity = 2f;
     public float idleThreshold = 0.1f;
+    public float friction = 0.9f;
+    public Vector3 pivotOffsetFromPlayer = new Vector3(0.3f, 1.8f, 0.0f);
 
     [Header("Mouse settings")]
     public float horizontalMouseSensitivity = 1f;
@@ -24,9 +27,32 @@ public class PlayerController : MonoBehaviour
     private float velocityZ = 0f;
     private bool isRunning = false;
     private float maxVelocity;
-    /*
-    
-    */
+    private bool isCameraOverRightShoulder = true;
+    private Vector3 switchShoulderVelocity;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        animator = GetComponent<Animator>();
+    }
+    void Update()
+    {
+        isRunning = Input.GetKey(KeyCode.LeftShift);
+        playerMoveDirection = (Input.GetAxis("Horizontal") * Vector2.right + Input.GetAxis("Vertical") * Vector2.up).normalized;
+        if (Input.GetKeyDown(KeyCode.Q))
+            isCameraOverRightShoulder = isCameraOverRightShoulder ? false : true;
+        pivot.transform.localPosition = Vector3.SmoothDamp(pivot.transform.localPosition,new Vector3(pivotOffsetFromPlayer.x * (isCameraOverRightShoulder ? 1f : -1f), pivot.transform.localPosition.y, pivot.transform.localPosition.z), ref switchShoulderVelocity, timeToSwitchShoulder);
+        
+        animator.SetFloat("VelocityX", velocityX);
+        animator.SetFloat("VelocityZ", velocityZ);
+        transform.LookAt(transform.position + new Vector3(Camera.main.transform.forward.x,0f, Camera.main.transform.forward.z), Vector3.up);
+    }
+    private void FixedUpdate()
+    {
+        maxVelocity = (isRunning && velocityZ >= -0.2f) ? maxRunningVelocity : maxWalkingVelocity; // maxVelocity set with respect to isRunning
+        changeVelocity();
+        capVelocity();
+        rb.velocity = Vector3.ClampMagnitude(transform.right * velocityX + transform.forward * velocityZ,maxVelocity) + rb.velocity.y * Vector3.up;
+    }
     private void capVelocity()
     {
         if (Mathf.Abs(velocityX) < idleThreshold)
@@ -42,40 +68,12 @@ public class PlayerController : MonoBehaviour
     {
         if (playerMoveDirection.x != 0)
             velocityX += playerMoveDirection.x * acceleration * Time.deltaTime;
-        else
-            velocityX -= Mathf.Sign(velocityX) * decceleration * Time.deltaTime;
-
-        if (playerMoveDirection.y != 0)
-        {
+        if (playerMoveDirection.y != 0){
             velocityZ += playerMoveDirection.y * acceleration * Time.deltaTime;
             velocityZ = Mathf.Max(velocityZ, -maxWalkingVelocity);
         }
-        else
-            velocityZ -= Mathf.Sign(velocityZ) * decceleration * Time.deltaTime;
+        velocityX *= friction;
+        velocityZ *= friction;
     }
-
-
-    void Start()
-    {
-        rb = GetComponent<Rigidbody>();
-        animator = GetComponent<Animator>();
-    }
-    void Update()
-    {
-        isRunning = Input.GetKey(KeyCode.LeftShift);
-        playerMoveDirection = Input.GetAxis("Horizontal") * Vector2.right + Input.GetAxis("Vertical") * Vector2.up;
-
-        //updateAimDownSights();
-        animator.SetFloat("VelocityX", velocityX);
-        animator.SetFloat("VelocityZ", velocityZ);
-        transform.LookAt(transform.position + new Vector3(Camera.main.transform.forward.x,0f, Camera.main.transform.forward.z), Vector3.up);
-    }
-
-    private void FixedUpdate()
-    {
-        maxVelocity = (isRunning && velocityZ >= -0.2f) ? maxRunningVelocity : maxWalkingVelocity; // maxVelocity set with respect to isRunning
-        changeVelocity();
-        capVelocity();
-        rb.velocity = Vector3.ClampMagnitude(transform.right * velocityX + transform.forward * velocityZ,maxVelocity) + rb.velocity.y * Vector3.up;
-    }
+    
 }
